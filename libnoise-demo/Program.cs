0c58f4ec6ba0af482a24ee3bc7bdf30d76abb87b
Lucas Klassmann <lucasklassmann@gmail.com>
2018-11-16 20:03:02 -0200

    Terrain texture generator example

--- libnoise-demo/Program.cs
+++ libnoise-demo/Program.cs
@@ -96,12 +96,58 @@ namespace libnoise_demo
             output.SaveToFile(filename);
         }
 
+        static void generateTerrainExample() {
+            const int SIZE = 1024;
+            var r = new Random();
+            var output = new ImageGenerator(SIZE, SIZE);
+            var colors = new Color[SIZE,SIZE];
+            var m = new Billow();
+
+            for (int x = 0; x < SIZE; x++) {
+                for (int y = 0; y < SIZE; y++) {
+                    var v = m.GetValue(x, 0.0, y);
+
+                    if (v < -0.35) {
+                        colors[x, y] = Color.FromArgb(255, 25, 47, 81);
+                    } else if (v < -0.25) {
+                        colors[x, y] = Color.FromArgb(255, 36, 79, 147);
+                    } else if (v < -0.15) {
+                        colors[x, y] = Color.FromArgb(255, 47, 96, 175);
+                    } else if (v < -0.1) {
+                        colors[x, y] = Color.FromArgb(255, 30, 96, 204);
+                    } else if (v < 0.0) {
+                        colors[x, y] = Color.FromArgb(255, 42, 117, 237);
+                    } else if (v < 0.1) {
+                        colors[x, y] = Color.FromArgb(255, 232, 227, 201);
+                    } else if (v < 0.2) {
+                        colors[x, y] = Color.FromArgb(255, 130, 142, 102);
+                    } else if (v < 0.5) {
+                        colors[x, y] = Color.FromArgb(255, 89, 132, 71);
+                    } else if (v < 0.7) {
+                        colors[x, y] = Color.FromArgb(255, 55, 89, 41);
+                    } else if (v < 0.8) {
+                        colors[x, y] = Color.FromArgb(255, 68, 81, 62);
+                    } else if (v < 0.85) {
+                        colors[x, y] = Color.FromArgb(255, 90, 91, 89);
+                    } else if (v < 0.90) {
+                        colors[x, y] = Color.FromArgb(255, 121, 122, 121);
+                    } else if (v <= 0.9995) {
+                        colors[x, y] = Color.FromArgb(255, 186, 188, 186);
+                    } else if (v <= 1.0 - 0.000001) {
+                        colors[x, y] = Color.FromArgb(255, 200, 200, 200);
+                    } else {
+                        colors[x, y] = Color.FromArgb(255, 242, 242, 242);
+                    }
+                }
+            }
+            output.Draw(colors);
+            output.SaveToFile("terrain.bmp");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("LibNoise");
-            // checkerBoard();
-            // perlin();
-            // voronoi();
+
             var perlin = new Perlin();
             perlin.OctaveCount = 1;
             perlin.Frequency = 0.05;
