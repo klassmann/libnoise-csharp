23e9e8c0b92263a94354ede12692438c1dd026dc
Lucas Klassmann <lucasklassmann@gmail.com>
2018-11-16 02:13:58 -0200

    CheckerBoard Console Example

--- libnoise-demo/Program.cs
+++ libnoise-demo/Program.cs
@@ -5,17 +5,31 @@ using noise.module;
 
 namespace libnoise_demo
 {
+
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Perlin p = new Perlin();
+
+        static void checkerBoard() {
+            Console.WriteLine("CheckerBoard:");
+            Module p = new CheckerBoard();
             for (double x = 0.0; x < 15.0; x++) {
-                for (double y = 0.0; y < 35.0; y++)
-                    Console.Write(p.GetValue(x, y, 1.0));
+                for (double y = 0.0; y < 35.0; y++) {
+                    var v = p.GetValue(x, y, 0.0);
+                    if (v == 1) {
+                        Console.Write("0");
+                    } else {
+                        Console.Write("X");
+                    }
+                }
+                    
                 Console.WriteLine();
             }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LibNoise");
+            checkerBoard();
         }
     }
 }
